518ac25fb422e8888f2c995879433dce750d101a
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-10-03 11:27:49 +0000

    More tests.

--- src/test/java/nl/vpro/poms/ApiScheduleTest.java
+++ src/test/java/nl/vpro/poms/ApiScheduleTest.java
@@ -7,10 +7,22 @@ import org.junit.Test;
 
 import nl.vpro.domain.api.ApiScheduleEvent;
 import nl.vpro.domain.api.media.ScheduleResult;
+import nl.vpro.domain.media.Channel;
+import nl.vpro.domain.media.Net;
 import nl.vpro.domain.media.Schedule;
+import nl.vpro.domain.user.Broadcaster;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
 
 
 public class ApiScheduleTest extends AbstractApiTest {
+
+
+    private static LocalDate today = LocalDate.now(Schedule.ZONE_ID);
+
+    static {
+        System.out.println("Today : " + today);
+    }
 
     public ApiScheduleTest() {
 
@@ -25,23 +37,36 @@ public class ApiScheduleTest extends AbstractApiTest {
 
     @Test
     public void list() throws Exception {
-        ScheduleResult o = clients.getScheduleService().list(LocalDate.now(Schedule.ZONE_ID), null, null, null, "ASC", 0L, 240);
+        ScheduleResult o = clients.getScheduleService().list(today, null, null, null, "ASC", 0L, 240);
+        assertThat(o.getSize()).isGreaterThan(10);
     }
 
     @Test
     public void listBroadcaster() throws Exception {
-        ScheduleResult o = clients.getScheduleService().listBroadcaster("VPRO", LocalDate.now(Schedule.ZONE_ID), null, null, null, "ASC", 0L, 240);
+        ScheduleResult o = clients.getScheduleService().listBroadcaster("VPRO", today, null, null, null, "ASC", 0L, 240);
+        assertThat(o.getSize()).isGreaterThan(10);
+        for (ApiScheduleEvent item : o.getItems()) {
+            assertThat(item.getMediaObject().getBroadcasters()).contains(new Broadcaster("VPRO"));
+        }
     }
 
 
     @Test
     public void listChannel() throws Exception {
-        ScheduleResult o = clients.getScheduleService().listChannel("NED1", LocalDate.now(Schedule.ZONE_ID), null, null, null, "ASC", 0L, 240);
+        ScheduleResult o = clients.getScheduleService().listChannel("NED1", today, null, null, null, "ASC", 0L, 240);
+        assertThat(o.getSize()).isGreaterThan(10);
+        for (ApiScheduleEvent item : o.getItems()) {
+            assertThat(item.getChannel()).isEqualTo(Channel.NED1);
+        }
     }
 
     @Test
     public void listNet() throws Exception {
-        ScheduleResult o = clients.getScheduleService().listNet("ZAPPNET", LocalDate.now(Schedule.ZONE_ID), null, null, null, "ASC", 0L, 240);
+        ScheduleResult o = clients.getScheduleService().listNet("ZAPP", today, null, null, null, "ASC", 0L, 240);
+        assertThat(o.getSize()).isGreaterThan(2);
+        for (ApiScheduleEvent item : o.getItems()) {
+            assertThat(item.getNet()).isEqualTo(new Net("ZAPP"));
+        }
     }
 
 
@@ -49,22 +74,35 @@ public class ApiScheduleTest extends AbstractApiTest {
     @Test
     public void nowForBroadcaster() throws Exception {
         ApiScheduleEvent o = clients.getScheduleService().nowForBroadcaster("VPRO", null);
+        assertThat(o.getMediaObject().getBroadcasters()).contains(new Broadcaster("VPRO"));
     }
 
     @Test
     public void nextForBroadcaster() throws Exception {
         ApiScheduleEvent o = clients.getScheduleService().nextForBroadcaster("VPRO", null);
+        System.out.println(o);
+        assertThat(o.getMediaObject().getBroadcasters()).contains(new Broadcaster("VPRO"));
+
+
     }
 
 
     @Test
     public void nowForChannel() throws Exception {
         ApiScheduleEvent o = clients.getScheduleService().nowForChannel("NED1", null);
+        System.out.println(o);
+        assertThat(o.getChannel()).isEqualTo(Channel.NED1);
+
+
     }
 
     @Test
     public void nextForChannel() throws Exception {
         ApiScheduleEvent o = clients.getScheduleService().nextForChannel("NED1", null);
+        System.out.println(o);
+        assertThat(o.getChannel()).isEqualTo(Channel.NED1);
+
+
     }
 
 
