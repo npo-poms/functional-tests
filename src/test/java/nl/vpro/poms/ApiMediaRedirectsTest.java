c30dada9332886410abc3613c5ae19356676b9d9
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-10-03 12:49:09 +0000

    Lets test the entire damn list.

--- src/test/java/nl/vpro/poms/ApiMediaRedirectsTest.java
+++ src/test/java/nl/vpro/poms/ApiMediaRedirectsTest.java
@@ -1,9 +1,14 @@
 package nl.vpro.poms;
 
+import java.io.IOException;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
 import javax.ws.rs.core.Response;
 
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import nl.vpro.domain.api.media.RedirectEntry;
 import nl.vpro.domain.api.media.RedirectList;
@@ -14,20 +19,27 @@ import static org.assertj.core.api.Java6Assertions.assertThat;
  * @author Michiel Meeuwissen
  * @since 1.0
  */
+@RunWith(Parameterized.class)
 public class ApiMediaRedirectsTest extends AbstractApiTest {
 
-    RedirectList list;
 
-    @Before
-    public void findRedirects() {
-        Response response = clients.getMediaService().redirects(null);
-        list = response.readEntity(RedirectList.class);
+    private final RedirectEntry entry;
+
+    public ApiMediaRedirectsTest(RedirectEntry entry) {
+        this.entry = entry;
     }
+
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> getDirects() throws IOException {
+        Response response = clients.getMediaService().redirects(null);
+        RedirectList list = response.readEntity(RedirectList.class);
+        return list.getList().stream().map(e -> new Object[]{e}).collect(Collectors.toList());
+    }
+
 
     @Test
     public void testRedirect() {
-        RedirectEntry entry = list.getList().get(0);
-
         assertThat(clients.getMediaService().load(entry.getFrom(), null, null).getMid()).isEqualTo(entry.getTo());
     }
 }
