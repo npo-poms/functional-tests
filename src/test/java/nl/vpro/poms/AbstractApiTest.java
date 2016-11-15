629c743ee61441e2572233ead5dad5e4971921a1
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-11-15 13:07:09 +0000

    Support for accept-headers.

--- src/test/java/nl/vpro/poms/AbstractApiTest.java
+++ src/test/java/nl/vpro/poms/AbstractApiTest.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.time.Duration;
 
+import javax.ws.rs.core.MediaType;
+
 import nl.vpro.api.client.resteasy.NpoApiClients;
 import nl.vpro.api.client.utils.NpoApiMediaUtil;
 import nl.vpro.rs.media.MediaRestClient;
@@ -18,7 +20,10 @@ public abstract class AbstractApiTest {
 
     protected static final Duration ACCEPTABLE_DURATION_FRONTEND = Duration.ofMinutes(10);
 
-    protected static final NpoApiClients clients = NpoApiClients.configured(Config.env(), Config.getProperties(Config.Prefix.npoapi)).build();
+    protected static final NpoApiClients clients =
+        NpoApiClients.configured(Config.env(), Config.getProperties(Config.Prefix.npoapi))
+            .mediaType(MediaType.APPLICATION_XML_TYPE)
+            .build();
     protected static final NpoApiMediaUtil mediaUtil;
     protected static final MediaRestClient backend = MediaRestClient.configured(Config.env(), Config.getProperties(Config.Prefix.backendapi)).build();
 
