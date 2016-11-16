6f28aa8069b122f63058316ee76741304e67efa4
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-11-16 13:41:14 +0000

    Not implemented exceptions are no test case errors.

--- src/test/java/nl/vpro/poms/npoapi/ApiScheduleSearchTest.java
+++ src/test/java/nl/vpro/poms/npoapi/ApiScheduleSearchTest.java
@@ -4,8 +4,13 @@ import java.io.IOException;
 import java.util.Collection;
 import java.util.function.Consumer;
 
+import javax.ws.rs.ServerErrorException;
+
 import org.junit.Assume;
+import org.junit.AssumptionViolatedException;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
@@ -17,6 +22,10 @@ import static org.assertj.core.api.Java6Assertions.assertThat;
 
 @RunWith(Parameterized.class)
 public class ApiScheduleSearchTest extends AbstractSearchTest<ScheduleForm, ScheduleSearchResult> {
+
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
     {
         TESTERS.put("facet-broadcaster-ned3.xml/null", sr -> {
@@ -37,13 +46,22 @@ public class ApiScheduleSearchTest extends AbstractSearchTest<ScheduleForm, Sche
     @Test
     public void search() throws Exception {
         System.out.println("--------------------" + name);
-        ScheduleSearchResult searchResultItems = clients.getScheduleService().find(form, profile, "", 0L, 10);
-        Consumer<ScheduleSearchResult> tester = TESTERS.get(name);
-        if (tester != null) {
-            System.out.println("USING  TESTER " + tester + " for " + name);
-            tester.accept(searchResultItems);
-        } else {
-            System.out.println("No predicate defined for " + name);
+        ScheduleSearchResult searchResultItems;
+        try {
+            searchResultItems = clients.getScheduleService().find(form, profile, "", 0L, 10);
+            Consumer<ScheduleSearchResult> tester = TESTERS.get(name);
+            if (tester != null) {
+                System.out.println("USING  TESTER " + tester + " for " + name);
+                tester.accept(searchResultItems);
+            } else {
+                System.out.println("No predicate defined for " + name);
+            }
+        } catch (ServerErrorException rs) {
+            if (rs.getResponse().getStatus() == 501) {
+                throw new AssumptionViolatedException(name + " seems to be not implemented yet");
+            } else {
+                throw rs;
+            }
         }
         test(name, searchResultItems);
 
