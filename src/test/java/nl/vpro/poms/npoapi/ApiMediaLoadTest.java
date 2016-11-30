ae95a352f72c5b217d95010f7ef5eb4f553dbb5f
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-11-30 15:17:34 +0000

    More default parameters and testing.

--- src/test/java/nl/vpro/poms/npoapi/ApiMediaLoadTest.java
+++ src/test/java/nl/vpro/poms/npoapi/ApiMediaLoadTest.java
@@ -44,6 +44,7 @@ public class ApiMediaLoadTest extends AbstractApiTest {
 
     @Before
     public void setup() {
+        clients.setProfile(profileName);
         if (profileName != null) {
             profile = clients.getProfileService().load(profileName, null);
         }
@@ -80,27 +81,13 @@ public class ApiMediaLoadTest extends AbstractApiTest {
         assumeTrue(mids.size() > 0);
         MediaObject o = clients.getMediaService().load(mids.get(0), null, null);
         assertThat(o.getMid()).isEqualTo(mids.get(0));
-        if (profileName != null) {
-            assertThat(profile.getMediaProfile().test(o)).isTrue();
+        if (clients.hasAllProperties()) {
+            if (profileName != null) {
+                assertThat(profile.getMediaProfile().test(o)).isTrue();
+            }
         }
     }
-
-    @Test
-    public void loadWithPropertiesNone() throws Exception {
-        assumeTrue(mids.size() > 0);
-        MediaObject o = clients.getMediaService().load(mids.get(0), "none", null);
-        assertThat(o.getMid()).isEqualTo(mids.get(0));
-    }
-
-    @Test
-    public void loadInProfile() throws Exception {
-        Assume.assumeNotNull(profileName);
-        assumeTrue(mids.size() > 0);
-        MediaObject o = clients.getMediaService().load(mids.get(0), null, profileName);
-        assertThat(o.getMid()).isEqualTo(mids.get(0));
-    }
-
-
+ 
     @Test
     public void loadOutsideProfile() throws Exception {
         Assume.assumeNotNull(profileName);
@@ -125,7 +112,7 @@ public class ApiMediaLoadTest extends AbstractApiTest {
 
         for (int i = 0; i < mids.size(); i++) {
             assertThat(o.getItems().get(i).getResult().getMid()).isEqualTo(mids.get(i));
-            if (profileName != null) {
+            if (profileName != null && clients.hasAllProperties()) {
                 assertThat(profile.getMediaProfile().test(o.getItems().get(i).getResult())).isTrue();
             }
         }
