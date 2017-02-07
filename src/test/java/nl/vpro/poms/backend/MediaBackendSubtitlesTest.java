e850e96ab1af7f3c5bf54282d11b661219e59a3f
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-02-07 14:49:39 +0000

    NFT-6

--- src/test/java/nl/vpro/poms/backend/MediaBackendSubtitlesTest.java
+++ src/test/java/nl/vpro/poms/backend/MediaBackendSubtitlesTest.java
@@ -6,10 +6,16 @@ import java.util.Locale;
 import org.junit.*;
 import org.junit.runners.MethodSorters;
 
+import nl.vpro.domain.subtitles.Cue;
+import nl.vpro.domain.subtitles.StandaloneCue;
 import nl.vpro.domain.subtitles.Subtitles;
+import nl.vpro.domain.subtitles.SubtitlesType;
 import nl.vpro.poms.AbstractApiMediaBackendTest;
 import nl.vpro.poms.DoAfterException;
+import nl.vpro.util.CountedIterator;
 
+import static nl.vpro.poms.Utils.waitUntilNotNull;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assume.assumeNoException;
 
 /**
@@ -45,7 +51,7 @@ public class MediaBackendSubtitlesTest extends AbstractApiMediaBackendTest {
                 "\n" +
                 "1\n" +
                 "00:00:02.200 --> 00:00:04.150\n" +
-                "888\n" +
+                "" + title + "\n" +
                 "\n" +
                 "2\n" +
                 "00:00:04.200 --> 00:00:08.060\n" +
@@ -60,5 +66,20 @@ public class MediaBackendSubtitlesTest extends AbstractApiMediaBackendTest {
         backend.setSubtitles(subtitles);
     }
 
+    @Test
+    public void checkArrived() throws Exception {
+        if (exception == null) {
+            Cue cue = waitUntilNotNull(ACCEPTABLE_DURATION, () -> {
+                CountedIterator<StandaloneCue> update = backend.getBackendRestService().getSubtitles(MID, Locale.CHINESE, SubtitlesType.TRANSLATION, true);
+                StandaloneCue found = update.next();
+                if (found.getContent().equals(title)) {
+                    return found;
+                }
+                return null;
 
+            });
+
+            assertThat(cue.getContent()).isEqualTo(title);
+        }
+    }
 }
