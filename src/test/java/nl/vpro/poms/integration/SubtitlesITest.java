0d09f69c65bbc60819e4bf4bd3efaeb5db793461
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-02-14 12:09:53 +0000

    NFT-6

--- src/test/java/nl/vpro/poms/integration/SubtitlesITest.java
+++ src/test/java/nl/vpro/poms/integration/SubtitlesITest.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.time.Duration;
+import java.time.Instant;
 import java.util.Locale;
 
 import org.junit.Before;
@@ -15,6 +16,7 @@ import com.google.common.collect.Iterators;
 import com.google.common.collect.PeekingIterator;
 
 import nl.vpro.api.client.utils.MediaRestClientUtils;
+import nl.vpro.domain.media.update.ProgramUpdate;
 import nl.vpro.domain.subtitles.StandaloneCue;
 import nl.vpro.domain.subtitles.Subtitles;
 import nl.vpro.domain.subtitles.SubtitlesUtil;
@@ -23,6 +25,9 @@ import nl.vpro.poms.AbstractApiMediaBackendTest;
 import static nl.vpro.poms.Utils.waitUntil;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.empty;
+
+import static org.hamcrest.Matchers.not;
 import static org.junit.Assume.assumeNotNull;
 import static org.junit.Assume.assumeThat;
 
@@ -44,9 +49,9 @@ public class SubtitlesITest extends AbstractApiMediaBackendTest {
     private static String firstTitle;
 
     @Test
-    public void test01addSubtitles() {
+    public void test01addSubtitles() throws IOException {
         assumeThat(backendVersionNumber, greaterThanOrEqualTo(5.1f));
-
+        assumeThat(mediaUtil.load(MID_WITH_LOCATIONS)[0].getLocations(), not(empty()));
 
         firstTitle = title;
 
@@ -87,6 +92,43 @@ public class SubtitlesITest extends AbstractApiMediaBackendTest {
             }
         , (pi) -> pi != null && pi.hasNext() && pi.peek().getContent().equals(firstTitle));
         assertThat(cueIterator).hasSize(3);
+    }
+
+
+    @Test
+    public void test03RevokeLocations() {
+        Instant now = Instant.now();
+        ProgramUpdate o = backend.get(MID_WITH_LOCATIONS);
+        o.getLocations().forEach(l -> {
+            l.setEmbargoStop(now);
+            }
+        );
+        backend.set(o);
+    }
+
+    @Test
+    public void test04WaitForInFrontend() throws Exception {
+        assumeNotNull(firstTitle);
+
+        waitUntil(ACCEPTABLE_DURATION, () -> mediaUtil.load(MID_WITH_LOCATIONS)[0].getLocations().isEmpty());
+
+        waitUntil(ACCEPTABLE_DURATION, () -> MediaRestClientUtils.loadOrNull(mediaUtil.getClients().getSubtitlesRestService(), MID_WITH_LOCATIONS, Locale.JAPAN) == null);
+    }
+
+    @Test
+    public void test05PublishLocations() {
+        Instant now = Instant.now();
+        ProgramUpdate o = backend.get(MID_WITH_LOCATIONS);
+        o.getLocations().forEach(l -> {
+                l.setEmbargoStop(null);
+            }
+        );
+        backend.set(o);
+    }
+
+    @Test
+    public void test06WaitForInFrontend() throws Exception {
+        test02WaitForInFrontend();
     }
 
 }
