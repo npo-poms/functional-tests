57155ae1f7e268e066119ea648a6e12a29003898
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-06-07 12:52:34 +0000

    Test for locations backend.

---
+++ src/test/java/nl/vpro/poms/backend/MediaBackendLocationsTest.java
@@ -0,0 +1,134 @@
+package nl.vpro.poms.backend;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.junit.*;
+import org.junit.runners.MethodSorters;
+
+import nl.vpro.domain.media.update.LocationUpdate;
+import nl.vpro.domain.media.update.ProgramUpdate;
+import nl.vpro.domain.media.update.collections.XmlCollection;
+import nl.vpro.poms.AbstractApiMediaBackendTest;
+import nl.vpro.poms.DoAfterException;
+
+import static nl.vpro.poms.Utils.waitUntil;
+import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.junit.Assume.assumeNoException;
+
+/**
+ * Tests whether adding and modifying locations via the POMS backend API works.
+ *
+ * @author Michiel Meeuwissen
+ */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@Slf4j
+public class MediaBackendLocationsTest extends AbstractApiMediaBackendTest {
+
+    private static final String MID = "WO_VPRO_025057";
+    private static final Duration ACCEPTABLE_DURATION = Duration.ofMinutes(3);
+    private static final List<String> titles = new ArrayList<>();
+
+    @Rule
+    public DoAfterException doAfterException = new DoAfterException((t) -> {
+        if (! (t instanceof AssumptionViolatedException)) {
+            MediaBackendLocationsTest.exception = t;
+        }
+    });
+
+    private static Throwable exception = null;
+
+    private String firstLocation;
+
+    @Before
+    public void setup() {
+        assumeNoException(exception);
+    }
+
+    @Test
+    public void test01addLocation() {
+        titles.add(title);
+        firstLocation = programUrl(title);
+        LocationUpdate update = LocationUpdate.builder()
+            .programUrl(firstLocation)
+            .build();
+        backend.addLocationToProgram(update, MID);
+    }
+
+
+    @Test
+    public void test10checkArrived() throws Exception {
+        List<String> currentLocations = new ArrayList<>();
+        waitUntil(ACCEPTABLE_DURATION,
+            MID + " in backend with location " + titles,
+            () -> {
+                ProgramUpdate update = backend.get(MID);
+                currentLocations.clear();
+                currentLocations.addAll(update.getLocations().stream().map(LocationUpdate::getProgramUrl).collect(Collectors.toList()));
+                return currentLocations.containsAll(titles.stream().map(this::programUrl).collect(Collectors.toSet()));
+            });
+
+        assertThat(currentLocations).containsAll(titles.stream().map(this::programUrl).collect(Collectors.toSet()));
+    }
+
+    @Test
+    public void test11checkArrivedViaGetLocations() throws Exception {
+        List<String> currentLocations = new ArrayList<>();
+        waitUntil(ACCEPTABLE_DURATION,
+            MID + " in backend with location " + titles,
+            () -> {
+                XmlCollection<LocationUpdate> update = backend.getBackendRestService().getLocations(null, MID, true);
+                currentLocations.clear();
+                currentLocations.addAll(update.stream().map(LocationUpdate::getProgramUrl).collect(Collectors.toList()));
+                return currentLocations.containsAll(titles.stream().map(this::programUrl).collect(Collectors.toSet()));
+            });
+
+        assertThat(currentLocations).containsAll(titles.stream().map(this::programUrl).collect(Collectors.toSet()));
+    }
+
+    @Test
+    public void test12updateLocation() throws IOException {
+        LocationUpdate update = backend.getBackendRestService().getLocations(null, MID, true).stream().filter(l -> l.getProgramUrl().equals(firstLocation)).findFirst().orElseThrow(IllegalStateException::new);
+
+        update.setProgramUrl(programUrl(title));
+
+
+        backend.addLocationToProgram(update, MID);
+    }
+
+    private String programUrl(String title) {
+        return "http://www.vpro.nl/" + title;
+    }
+
+
+
+    @Test
+    public void test98Cleanup() throws Exception {
+        backend.getBrowserCache().clear();
+        ProgramUpdate update = backend.get(MID);
+        log.info("Removing locations " + update.getLocations());
+        update.getLocations().clear();
+        backend.set(update);
+    }
+
+
+    @Test
+    public void test99CleanupCheck() throws Exception {
+        final ProgramUpdate[] update = new ProgramUpdate[1];
+        waitUntil(ACCEPTABLE_DURATION,
+            MID + " has no locations any more",
+            () -> {
+                update[0] = backend.get(MID);
+                return update[0].getLocations().isEmpty();
+            });
+        assertThat(update[0].getLocations()).isEmpty();
+    }
+
+
+
+}
