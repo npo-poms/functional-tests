f8bda8d26a399dbff82b9ea93b486dc451ec4c31
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-06-07 11:28:27 +0000

    Added a tester.

--- src/test/java/nl/vpro/poms/npoapi/ApiSubtitlesParameterizedSearchTest.java
+++ src/test/java/nl/vpro/poms/npoapi/ApiSubtitlesParameterizedSearchTest.java
@@ -9,10 +9,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
+import nl.vpro.domain.api.SearchResultItem;
 import nl.vpro.domain.api.subtitles.SubtitlesForm;
 import nl.vpro.domain.api.subtitles.SubtitlesSearchResult;
+import nl.vpro.domain.subtitles.StandaloneCue;
 import nl.vpro.poms.ApiSearchTestHelper;
 
+import static org.assertj.core.api.Java6Assertions.assertThat;
 import static org.junit.Assume.assumeTrue;
 
 @RunWith(Parameterized.class)
@@ -20,6 +23,13 @@ import static org.junit.Assume.assumeTrue;
 public class ApiSubtitlesParameterizedSearchTest extends AbstractSearchTest<SubtitlesForm, SubtitlesSearchResult> {
 
 
+    {
+        addTester("bla.json/(xml|json)", sr -> {
+            for (SearchResultItem<? extends StandaloneCue> m : sr.getItems()) {
+                assertThat(m.getResult().getContent()).containsIgnoringCase("bla");
+            }
+        });
+    }
     public ApiSubtitlesParameterizedSearchTest(String name, SubtitlesForm form, javax.ws.rs.core.MediaType mediaType) {
         super(name, form, null, mediaType);
     }
