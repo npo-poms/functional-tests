c73dc978acdab3f8ae1ea3738ef85dc347f7e42f
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-05-17 21:39:57 +0000

    Fixed failing test case.

--- src/test/java/nl/vpro/poms/client/BackendApiClientTest.java
+++ src/test/java/nl/vpro/poms/client/BackendApiClientTest.java
@@ -15,7 +15,7 @@ public class BackendApiClientTest extends AbstractApiMediaBackendTest  {
     @Test
     public void getByCrid() {
         MediaUpdate<?> mediaUpdate = backend.get("crid://tmp.fragment.mmbase.vpro.nl/43084334");
-        assertThat(mediaUpdate).isNotNull(); // fails
-        assertThat(mediaUpdate.getMid()).isEqualTo("");
+        assertThat(mediaUpdate).isNotNull();
+        assertThat(mediaUpdate.getMid()).isEqualTo("WO_VPRO_034420");
     }
 }
