b275500cf4af445d970724219c5acd4eb8109bf5
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-05-17 21:09:34 +0000

    Slashes in paths are not working in current client.

---
+++ src/test/java/nl/vpro/poms/client/BackendApiClientTest.java
@@ -0,0 +1,21 @@
+package nl.vpro.poms.client;
+
+import org.junit.Test;
+
+import nl.vpro.domain.media.update.MediaUpdate;
+import nl.vpro.poms.AbstractApiMediaBackendTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Michiel Meeuwissen
+ */
+public class BackendApiClientTest extends AbstractApiMediaBackendTest  {
+
+    @Test
+    public void getByCrid() {
+        MediaUpdate<?> mediaUpdate = backend.get("crid://tmp.fragment.mmbase.vpro.nl/43084334");
+        assertThat(mediaUpdate).isNotNull(); // fails
+        assertThat(mediaUpdate.getMid()).isEqualTo("");
+    }
+}
