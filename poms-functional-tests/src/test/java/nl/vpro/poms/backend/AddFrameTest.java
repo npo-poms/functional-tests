6f9348cbec44bcfb15161b13e4513dca45723cff
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-11-26 10:56:00 +0100

    Testing

--- poms-functional-tests/src/test/java/nl/vpro/poms/backend/AddFrameTest.java
+++ poms-functional-tests/src/test/java/nl/vpro/poms/backend/AddFrameTest.java
@@ -3,12 +3,15 @@ package nl.vpro.poms.backend;
 import lombok.extern.slf4j.Slf4j;
 
 import java.time.Duration;
+import java.util.stream.Collectors;
 
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
 import nl.vpro.domain.image.ImageType;
+import nl.vpro.domain.media.Program;
+import nl.vpro.domain.media.support.OwnerType;
 import nl.vpro.domain.media.update.ProgramUpdate;
 import nl.vpro.poms.AbstractApiMediaBackendTest;
 
@@ -48,6 +51,27 @@ public class AddFrameTest extends AbstractApiMediaBackendTest {
 
     @Test
     public void test98Cleanup() {
+        ProgramUpdate update = backend_authority.get(MID);
+        assumeTrue(update != null);
+        log.info("Removing images " + update.getImages());
+        update.getImages().clear();
+        backend_authority.set(update);
+
+    }
+
+
+    @Test
+    public void test99CheckCleanup() {
+         waitUntil(ACCEPTABLE_DURATION,
+            MID + " has no stills",
+            () -> {
+                Program p = backend.getFullProgram(MID);
+                return p != null &&
+                    p.getImages()
+                        .stream()
+                        .filter(iu -> iu != null && iu.getOwner() == OwnerType.AUTHORITY && iu.getType() == ImageType.STILL)
+                        .collect(Collectors.toList()).size() == 0;
+            });
         ProgramUpdate update = backend_authority.get(MID);
         assumeTrue(update != null);
         log.info("Removing images " + update.getImages());
