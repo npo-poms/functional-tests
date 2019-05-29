88e49292faaf38ff785f1bfe28d777a3376f9a9f
Bert-Jan ter Hofte <bertjh@specialisterren.nl>
2019-05-29 13:27:31 +0200

    update media selector cms

--- poms-functional-tests-selenium/src/test/java/nl/vpro/poms/selenium/poms/CMSSelector/CMSSelectorTest.java
+++ poms-functional-tests-selenium/src/test/java/nl/vpro/poms/selenium/poms/CMSSelector/CMSSelectorTest.java
@@ -1,5 +1,8 @@
 package nl.vpro.poms.selenium.poms.CMSSelector;
 
+import nl.vpro.poms.selenium.pages.CMSMediaSelector;
+import nl.vpro.poms.selenium.pages.MediaItemPage;
+import nl.vpro.poms.selenium.pages.Search;
 import nl.vpro.poms.selenium.poms.AbstractTest;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,6 +13,14 @@ public class CMSSelectorTest extends AbstractTest {
     public void SPOMSCMSSELECTOR1(){
         loginOmroepGebruiker();
         logout();
-
+        openCMSSelectorPage();
+        CMSMediaSelector cms = new CMSMediaSelector(driver);
+        cms.clickButtonSelect();
+//        Hier gebleven geeft error uitzoeken
+        cms.switchToPomsWindows();
+        Search search = new Search(driver);
+        search.clickRow(0);
     }
+
+
 }
