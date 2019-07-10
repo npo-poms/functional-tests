5411f2a8d64f73a0fcba60574992806b9dfa78eb
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-07-10 15:35:42 +0200

    Finalized some members.

--- poms-functional-tests-selenium/src/test/java/nl/vpro/poms/selenium/pages/AbstractPage.java
+++ poms-functional-tests-selenium/src/test/java/nl/vpro/poms/selenium/pages/AbstractPage.java
@@ -1,18 +1,20 @@
 package nl.vpro.poms.selenium.pages;
 
 import lombok.extern.slf4j.Slf4j;
-import nl.vpro.poms.selenium.util.WebDriverUtil;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import nl.vpro.poms.selenium.util.WebDriverUtil;
 
 @Slf4j
 public abstract class AbstractPage {
 
-    WebDriver driver;
-    WebDriverUtil waitUtil;
+    final WebDriver driver;
+    final WebDriverUtil waitUtil;
 	WebDriverWait wait;
 
-    
+
     protected AbstractPage(WebDriver driver) {
         this.waitUtil = new WebDriverUtil(driver);
         this.driver = driver;
