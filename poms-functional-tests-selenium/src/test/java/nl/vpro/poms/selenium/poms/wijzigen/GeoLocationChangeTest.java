9dbaf1d45e27e4f27573988da4e4d2098f8a02b9
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-08-14 14:27:17 +0200

    NPA-491 Started on a test for the POMS side.

---
+++ poms-functional-tests-selenium/src/test/java/nl/vpro/poms/selenium/poms/wijzigen/GeoLocationChangeTest.java
@@ -0,0 +1,41 @@
+package nl.vpro.poms.selenium.poms.wijzigen;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+import nl.vpro.poms.selenium.pages.MediaItemPage;
+import nl.vpro.poms.selenium.pages.Search;
+import nl.vpro.poms.selenium.poms.AbstractTest;
+import nl.vpro.poms.selenium.util.WebDriverFactory;
+
+public class GeoLocationChangeTest extends AbstractTest {
+
+	public GeoLocationChangeTest(WebDriverFactory.Browser browser) {
+		super(browser);
+	}
+
+	@Before
+	public void firstLogin() {
+		login().speciaalVf();
+	}
+	@After
+	public void afterwardsLogout() {
+		logout();
+	}
+
+
+	@Test
+	public void clearGeoLocations()  {
+		MediaItemPage item = new Search(webDriverUtil).searchAndOpenClip();
+		item.moveToElement(By.id("geolocations"));
+
+		// now clear it an add another one.
+
+		// Then check wether submitting works.
+
+	}
+
+
+}
