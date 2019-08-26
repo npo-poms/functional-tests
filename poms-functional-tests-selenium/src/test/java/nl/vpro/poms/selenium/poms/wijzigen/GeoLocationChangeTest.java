e44f237b127a14e9cc837f2b96fe8116c4f74eaf
Giorgio Vinci <giorgiovinci@gmail.com>
2019-08-26 16:20:45 +0200

    NPA-491

--- poms-functional-tests-selenium/src/test/java/nl/vpro/poms/selenium/poms/wijzigen/GeoLocationChangeTest.java
+++ poms-functional-tests-selenium/src/test/java/nl/vpro/poms/selenium/poms/wijzigen/GeoLocationChangeTest.java
@@ -1,5 +1,6 @@
 package nl.vpro.poms.selenium.poms.wijzigen;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Ignore;
@@ -10,6 +11,12 @@ import nl.vpro.poms.selenium.pages.MediaItemPage;
 import nl.vpro.poms.selenium.pages.Search;
 import nl.vpro.poms.selenium.poms.AbstractTest;
 import nl.vpro.poms.selenium.util.WebDriverFactory;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.openqa.selenium.By.*;
 
 @Ignore("TODO")
 public class GeoLocationChangeTest extends AbstractTest {
@@ -17,6 +24,7 @@ public class GeoLocationChangeTest extends AbstractTest {
 	public GeoLocationChangeTest(WebDriverFactory.Browser browser) {
 		super(browser);
 	}
+	private static final String POPUP_TITLE = "GTAA";
 
 	@Before
 	public void firstLogin() {
@@ -31,13 +39,38 @@ public class GeoLocationChangeTest extends AbstractTest {
 	@Test
 	public void clearGeoLocations()  {
 		MediaItemPage item = new Search(webDriverUtil).searchAndOpenClip();
+		String poms_title = driver.getTitle();
 		item.moveToElement(By.id("geolocations"));
 
-		// now clear it an add another one.
+		//Remove eventual geolocations
+//		List<WebElement> removeButtons = driver.findElement(By.xpath("//poms-geolocations")).findElements(By.className("media-concept-remove"));
+//		for(WebElement button: removeButtons){
+//			button.click();
+//			webDriverUtil.waitForAngularRequestsToFinish();
+//		}
+//		assertThat(removeButtons.size()).isEqualTo(0);
+
+		WebElement addButton = driver.findElement(By.id("addGeoLocation"));
+		addButton.click();
+		webDriverUtil.waitForAngularRequestsToFinish();
+		webDriverUtil.switchToWindowWithTitle(POPUP_TITLE);
+		waitUntilSuggestionReady();
+		// first suggestion should be it
+		WebElement webElement = driver.findElements(By.xpath("//ul/li")).get(0).findElement(tagName("a"));
+		webElement.click();
+		webDriverUtil.click("submit");
+		webDriverUtil.waitForWindowToClose();
+		webDriverUtil.switchToWindowWithTitle(poms_title);
 
 		// Then check wether submitting works.
+ 		List<WebElement> newRemoveButtons = driver.findElement(By.xpath("//poms-geolocations")).findElements(By.className("media-concept-remove"));
+		assertThat(newRemoveButtons.size()).isEqualTo(1);
 
 	}
 
+	private void waitUntilSuggestionReady() {
+		wait.until(webDriver ->
+				! webDriver.findElement(id("searchValue")).getAttribute("class").contains("waiting"));
+	}
 
 }
