d70e2def712c0db287a6d7690404e913ebc45c34
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2020-01-13 23:30:50 +0100

    Details.

--- poms-functional-tests-apis/src/test/java/nl/vpro/poms/backend/MediaBackendAddFieldValues.java
+++ poms-functional-tests-apis/src/test/java/nl/vpro/poms/backend/MediaBackendAddFieldValues.java
@@ -4,15 +4,21 @@ import lombok.extern.log4j.Log4j2;
 
 import java.net.URI;
 import java.time.Duration;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Objects;
 
 import org.junit.jupiter.api.*;
 
 import nl.vpro.domain.media.*;
-import nl.vpro.domain.media.support.OwnerType;
 import nl.vpro.domain.media.update.ProgramUpdate;
 import nl.vpro.poms.AbstractApiMediaBackendTest;
 
+import static nl.vpro.domain.media.GeoRoleType.SUBJECT;
+import static nl.vpro.domain.media.IntentionType.ENTERTAINMENT_INFORMATIVE;
+import static nl.vpro.domain.media.IntentionType.INFORM_INDEPTH;
+import static nl.vpro.domain.media.TargetGroupType.KIDS_12;
+import static nl.vpro.domain.media.TargetGroupType.KIDS_6;
+import static nl.vpro.domain.media.support.OwnerType.BROADCASTER;
 import static nl.vpro.testutils.Utils.waitUntil;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -26,7 +32,7 @@ public class MediaBackendAddFieldValues extends AbstractApiMediaBackendTest {
     private static final Duration ACCEPTABLE_DURATION = Duration.ofMinutes(3);
 
     static Program created;
-    static String mid = "POMS_VPRO_3319601";
+    static String mid = "POMS_VPRO_3319601"; // just t be able to run a single test. just change this in the last one if a test needs fixing.
 
     @BeforeEach
     public void init() {
@@ -53,19 +59,16 @@ public class MediaBackendAddFieldValues extends AbstractApiMediaBackendTest {
         addTopics(builder);
         addGeolocations(builder);
 
-
         ProgramUpdate clip = ProgramUpdate.create(builder.build());
 
         //When we save the media
         mid = backend.set(clip);
         log.info("Found mid {}", mid);
 
-
         created = waitUntil(ACCEPTABLE_DURATION,
                 mid + " exists",
                 () -> backend.getFull(mid),
                 Objects::nonNull);
-
     }
 
 
@@ -74,8 +77,8 @@ public class MediaBackendAddFieldValues extends AbstractApiMediaBackendTest {
         builder.intentions(
             Intentions.builder()
                 .values(Arrays.asList(
-                    IntentionType.ENTERTAINMENT_INFORMATIVE,
-                    IntentionType.INFORM_INDEPTH))
+                    ENTERTAINMENT_INFORMATIVE,
+                    INFORM_INDEPTH))
                 .build()
         );
     }
@@ -84,16 +87,16 @@ public class MediaBackendAddFieldValues extends AbstractApiMediaBackendTest {
     @Order(10)
     public void checkIntentions() {
         assertThat(created.getIntentions()).hasSize(1);
-        assertThat(created.getIntentions().first().getOwner()).isEqualTo(OwnerType.BROADCASTER);
-        assertThat(created.getIntentions().first().getValues().stream().map(Intention::getValue)).containsExactly(IntentionType.ENTERTAINMENT_INFORMATIVE, IntentionType.INFORM_INDEPTH);
-
-
+        assertThat(created.getIntentions().first().getOwner()).isEqualTo(BROADCASTER);
+        assertThat(created.getIntentions().first()
+            .getValues().stream().map(Intention::getValue))
+            .containsExactly(ENTERTAINMENT_INFORMATIVE, INFORM_INDEPTH);
     }
 
     private void addTargetGroups(MediaBuilder.ProgramBuilder  builder) {
         builder.targetGroups(
             TargetGroups.builder()
-                .values(Arrays.asList(TargetGroupType.KIDS_6, TargetGroupType.KIDS_12))
+                .values(Arrays.asList(KIDS_6, KIDS_12))
                 .build()
         );
     }
@@ -102,10 +105,10 @@ public class MediaBackendAddFieldValues extends AbstractApiMediaBackendTest {
     @Order(11)
     public void checkTargetGroups() {
         assertThat(created.getTargetGroups()).hasSize(1);
-        assertThat(created.getTargetGroups().first().getOwner()).isEqualTo(OwnerType.BROADCASTER);
-        assertThat(created.getTargetGroups().first().getValues().stream().map(TargetGroup::getValue)).containsExactly(TargetGroupType.KIDS_6, TargetGroupType.KIDS_12);
-
-
+        assertThat(created.getTargetGroups().first().getOwner()).isEqualTo(BROADCASTER);
+        assertThat(created.getTargetGroups().first()
+            .getValues().stream().map(TargetGroup::getValue))
+            .containsExactly(KIDS_6, KIDS_12);
     }
 
 
@@ -114,38 +117,39 @@ public class MediaBackendAddFieldValues extends AbstractApiMediaBackendTest {
             URI.create("http://data.beeldengeluid.nl/gtaa/25890"), // honden
             URI.create("http://data.beeldengeluid.nl/gtaa/26526") // planeten
         );
-
     }
 
     @Test
     @Order(12)
     public void checkTopics() {
         assertThat(created.getTopics()).hasSize(1);
-        assertThat(created.getTopics().first().getOwner()).isEqualTo(OwnerType.BROADCASTER);
+        assertThat(created.getTopics().first().getOwner()).isEqualTo(BROADCASTER);
         assertThat(created.getTopics().first().getValues()
-            .stream().map(Topic::getName)).containsExactly("honden", "planeten");
-
+            .stream().map(Topic::getName))
+            .containsExactly("honden", "planeten");
     }
 
     private void addGeolocations(MediaBuilder.ProgramBuilder  builder) {
-        builder.geoLocations(GeoRoleType.SUBJECT,
+        builder.geoLocations(SUBJECT,
             URI.create("http://data.beeldengeluid.nl/gtaa/43919"), // ulft
             URI.create("http://data.beeldengeluid.nl/gtaa/43996") // utrecht
         );
-
     }
+
     @Test
     @Order(13)
     public void checkGeolocations() {
         assertThat(created.getGeoLocations()).hasSize(1);
-        assertThat(created.getGeoLocations().first().getOwner()).isEqualTo(OwnerType.BROADCASTER);
+        assertThat(created.getGeoLocations().first().getOwner()).isEqualTo(BROADCASTER);
         assertThat(created.getGeoLocations().first().getValues()
-            .stream().map(GeoLocation::getName)).containsExactly("Ulft", "Utrecht (stad)");
+            .stream().map(GeoLocation::getName))
+            .containsExactly("Ulft", "Utrecht (stad)");
         assertThat(created.getGeoLocations().first().getValues()
-            .stream().map(GeoLocation::getRole)).containsExactly(GeoRoleType.SUBJECT, GeoRoleType.SUBJECT);
-
+            .stream().map(GeoLocation::getRole))
+            .containsExactly(SUBJECT, SUBJECT);
     }
 
+    @SuppressWarnings("UnusedReturnValue")
     private Program getCreated() {
         if (created == null && mid != null) {
             created = backend.getFull(mid);
