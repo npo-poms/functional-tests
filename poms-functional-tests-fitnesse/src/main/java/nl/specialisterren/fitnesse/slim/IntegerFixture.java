1d16df15d05d04e13010a56783f055b70b84e3ab
Ronny Gunawan <ronnyg@specialisterren.nl>
2020-02-26 16:58:22 +0100

    Voeg script 'Npo Ta 23: Spoms zoek 3' toe

--- poms-functional-tests-fitnesse/src/main/java/nl/specialisterren/fitnesse/slim/IntegerFixture.java
+++ poms-functional-tests-fitnesse/src/main/java/nl/specialisterren/fitnesse/slim/IntegerFixture.java
@@ -4,4 +4,13 @@ public class IntegerFixture {
 	public boolean valueIsSmallerThan(int a, int b) {
 		return a < b;
 	}
+	
+    public int subtractAnd(String a, int b) {
+        Integer x = null;
+        if (a != null) {
+            x = Integer.valueOf(a);
+        }
+		
+        return x - b;
+    }
 }
