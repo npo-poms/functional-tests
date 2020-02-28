f0456209e7bc9e4f55235205c9a8fc2aee5bf5ab
Ronny Gunawan <ronnyg@specialisterren.nl>
2020-02-28 16:49:50 +0100

    Voeg script 'Npo Ta 24: Spoms zoek 4' toe + update rest

--- poms-functional-tests-fitnesse/src/main/java/nl/specialisterren/fitnesse/slim/CalendarFixture.java
+++ poms-functional-tests-fitnesse/src/main/java/nl/specialisterren/fitnesse/slim/CalendarFixture.java
@@ -6,6 +6,9 @@ import java.util.Date;
 import java.text.SimpleDateFormat;
 import java.text.ParseException;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class CalendarFixture {
 	public String decreaseTodayWithMonthsWithFormatWithLocal(String months, String format, String local) throws ParseException {
         Calendar cal = Calendar.getInstance();
@@ -15,4 +18,25 @@ public class CalendarFixture {
 		cal.add(Calendar.DAY_OF_YEAR, 1);
         return new SimpleDateFormat(format,loc).format(cal.getTime());
     }
+	
+	public String increaseDateWithHours(String datetime, int hours) {
+		String regex = "(?<day>\\d{2})-(?<month>\\d{2})-(?<year>\\d{4}) (?<hour>\\d{2}):(?<minute>\\d{2})";
+		Pattern p = Pattern.compile(regex);
+		Matcher m = p.matcher(datetime);
+
+		if (!m.find())
+			return "";
+		
+		int day = Integer.valueOf(m.group("day"));
+		int month = Integer.valueOf(m.group("month"));
+		int year = Integer.valueOf(m.group("year"));
+		int hour = Integer.valueOf(m.group("hour"));
+		int minute = Integer.valueOf(m.group("minute"));
+		
+        Calendar cal = Calendar.getInstance();
+		cal.set(year, month-1, day, hour, minute);
+		cal.add(Calendar.HOUR, hours);
+
+		return new SimpleDateFormat("dd-MM-yyyy HH:mm", new Locale("nl")).format(cal.getTime());
+	}
 }
--- poms-functional-tests-fitnesse/src/main/java/nl/specialisterren/fitnesse/slim/web/ExtendedBrowserTest.java
+++ poms-functional-tests-fitnesse/src/main/java/nl/specialisterren/fitnesse/slim/web/ExtendedBrowserTest.java
@@ -1,7 +1,7 @@
 package nl.specialisterren.fitnesse.fixture.slim.web;
 
 import nl.hsac.fitnesse.fixture.slim.web.BrowserTest;
-
+import nl.hsac.fitnesse.fixture.slim.web.annotation.TimeoutPolicy;
 import nl.hsac.fitnesse.fixture.slim.web.annotation.WaitUntil;
 import org.openqa.selenium.*;
 
@@ -9,4 +9,9 @@ public class ExtendedBrowserTest extends BrowserTest {
 	public Object store(Object result) {
 		return result;
 	}
+	
+	@WaitUntil(TimeoutPolicy.RETURN_FALSE)
+    public boolean waitForNotVisible(String place) {
+        return !waitForVisibleIn(place, null);
+    }
 }
