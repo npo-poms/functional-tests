21220258cda72825170cd7ce8babe04e36e532a7
Ronny Gunawan <ronnyg@specialisterren.nl>
2020-03-03 15:11:36 +0100

    Voeg script 'Npo Ta 27: Spoms edit upload 1' toe

--- poms-functional-tests-fitnesse/src/main/java/nl/specialisterren/fitnesse/slim/IntegerFixture.java
+++ poms-functional-tests-fitnesse/src/main/java/nl/specialisterren/fitnesse/slim/IntegerFixture.java
@@ -5,6 +5,15 @@ public class IntegerFixture {
 		return a < b;
 	}
 	
+	public int addAnd(String a, int b) {
+        Integer x = null;
+        if (a != null) {
+            x = Integer.valueOf(a);
+        }
+		
+        return x + b;
+    }
+	
     public int subtractAnd(String a, int b) {
         Integer x = null;
         if (a != null) {
