efaf4fbd8df060f10480604c5fd610efb4b80874
Ronny Gunawan <ronnyg@specialisterren.nl>
2020-03-12 15:48:22 +0100

    Voeg fixture 'remove last search context' toe

--- poms-functional-tests-fitnesse/src/main/java/nl/specialisterren/fitnesse/slim/web/ExtendedBrowserTest.java
+++ poms-functional-tests-fitnesse/src/main/java/nl/specialisterren/fitnesse/slim/web/ExtendedBrowserTest.java
@@ -5,6 +5,8 @@ import nl.hsac.fitnesse.fixture.slim.web.annotation.TimeoutPolicy;
 import nl.hsac.fitnesse.fixture.slim.web.annotation.WaitUntil;
 import org.openqa.selenium.*;
 
+import java.util.List;
+
 public class ExtendedBrowserTest extends BrowserTest {
 	public Object store(Object result) {
 		return result;
@@ -17,5 +19,17 @@ public class ExtendedBrowserTest extends BrowserTest {
 	
 	public boolean isPresentOnPage(String place) {
         return isVisibleOnPage(place);
+    }
+	
+	public void removeLastSearchContext() {
+        List<String> currentSearchContextPath = getCurrentSearchContextPath();
+		int size = currentSearchContextPath.size();
+        
+        if (size == 1)
+            clearSearchContext();
+        else if (size > 1) {
+            currentSearchContextPath.remove(size-1);          
+            setSearchContextTo(currentSearchContextPath.get(size-2));
+        }
     }
 }
