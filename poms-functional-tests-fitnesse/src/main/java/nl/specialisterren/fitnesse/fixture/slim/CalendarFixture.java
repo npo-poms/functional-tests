ff795e14baf9d507a8deb94b61d276babc4336cd
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2020-04-29 10:59:35 +0200

    Just added some TODO's

--- poms-functional-tests-fitnesse/src/main/java/nl/specialisterren/fitnesse/fixture/slim/CalendarFixture.java
+++ poms-functional-tests-fitnesse/src/main/java/nl/specialisterren/fitnesse/fixture/slim/CalendarFixture.java
@@ -1,30 +1,30 @@
 package nl.specialisterren.fitnesse.fixture.slim;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
-import java.util.Date;
-import java.text.SimpleDateFormat;
-import java.text.ParseException;
-
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+
+// TODO useless use of  boxing
 public class CalendarFixture {
 	public String decreaseTodayWithMonthsWithFormatWithLocal(String months, String format, String local) throws ParseException {
         Calendar cal = Calendar.getInstance();
         Locale loc = new Locale(local);
-		
+
         Integer intMonths = Integer.valueOf(months);
         cal.add(Calendar.DAY_OF_YEAR, -intMonths*30);
-		
+
 		int currentMonth = cal.get(Calendar.MONTH);
 		int currentYear = cal.get(Calendar.YEAR);
 		if (currentYear % 4 == 0 && currentMonth == 3)
 		    cal.add(Calendar.DAY_OF_YEAR, -1);
-        
+
 		return new SimpleDateFormat(format,loc).format(cal.getTime());
     }
-	
+
 	public String increaseDateWithHours(String datetime, int hours) {
 		String regex = "(?<day>\\d{2})-(?<month>\\d{2})-(?<year>\\d{4}) (?<hour>\\d{2}):(?<minute>\\d{2})";
 		Pattern p = Pattern.compile(regex);
@@ -32,13 +32,13 @@ public class CalendarFixture {
 
 		if (!m.find())
 			return "";
-		
+
 		int day = Integer.valueOf(m.group("day"));
 		int month = Integer.valueOf(m.group("month"));
 		int year = Integer.valueOf(m.group("year"));
 		int hour = Integer.valueOf(m.group("hour"));
 		int minute = Integer.valueOf(m.group("minute"));
-		
+
         Calendar cal = Calendar.getInstance();
 		cal.set(year, month-1, day, hour, minute);
 		cal.add(Calendar.HOUR, hours);
