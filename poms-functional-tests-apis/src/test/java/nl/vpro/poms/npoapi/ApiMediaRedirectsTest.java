939dfbb299575a8496b1f092476e03af31dbf406
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2020-04-29 12:19:54 +0200

    Fixing test cases on test.

--- poms-functional-tests-apis/src/test/java/nl/vpro/poms/npoapi/ApiMediaRedirectsTest.java
+++ poms-functional-tests-apis/src/test/java/nl/vpro/poms/npoapi/ApiMediaRedirectsTest.java
@@ -2,16 +2,19 @@ package nl.vpro.poms.npoapi;
 
 import lombok.extern.log4j.Log4j2;
 
-import java.util.Collection;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import javax.ws.rs.core.Response;
 
 import org.assertj.core.api.Assumptions;
 import org.assertj.core.api.Fail;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
+
+import com.google.common.collect.Lists;
 
 import nl.vpro.domain.api.media.RedirectEntry;
 import nl.vpro.domain.api.media.RedirectList;
@@ -30,15 +33,37 @@ import static org.assertj.core.api.Assertions.assertThat;
 class ApiMediaRedirectsTest extends AbstractApiTest {
 
     static Collection<Object[]> getRedirects() {
+        RedirectList list = getRedirectsList();
+        return list.getList()
+            .stream()
+            .map(e -> new Object[]{e})
+            .collect(Collectors.toList());
+    }
+
+    static RedirectList getRedirectsList() {
         try (Response response = clients.getMediaService().redirects(null)) {
             assertThat(response.getStatus()).isEqualTo(200);
             RedirectList list = response.readEntity(RedirectList.class);
             assertThat(list).isNotEmpty();
-            return list.getList()
-                .stream()
-                .map(e -> new Object[]{e})
-                .collect(Collectors.toList());
+            return list;
         }
+    }
+
+    @Test
+    @Disabled("Used to find a the mids to republish to fix NPA-535 (for tests)")
+    public void showRedirects() {
+        Set<String> mids = new HashSet<>();
+        getRedirectsList().forEach(re -> {
+            mids.add(re.getFrom());
+            mids.add(re.getTo());
+        });
+        List<String> list = new ArrayList<>(mids);
+        List<List<String>> partition = Lists.partition(list, 200);
+        log.info("relevant mids: {}", list.size());
+        for (List<String> p : partition) {
+            log.info("{}", String.join(",", p));
+        }
+
     }
 
     @ParameterizedTest
@@ -53,9 +78,10 @@ class ApiMediaRedirectsTest extends AbstractApiTest {
             if (! Objects.equals(entry.getTo(), entry.getUltimate())) {
                 Assumptions.assumeThat(apiVersionNumber)
                     .withFailMessage("Known to fail before 5.12. See NPA-533/ %s", entry)
-                    .isGreaterThanOrEqualTo(IntegerVersion.of(5, 12));
+                    .isGreaterThanOrEqualTo(IntegerVersion.of(5, 12, 2));
             } else {
                 // TODO: Still suffering https://jira.vpro.nl/browse/NPA-535
+                log.info("NPA-535");
             }
             Fail.fail("Could not resolve %s: %s", entry, nfe.getMessage(),  nfe);
 
