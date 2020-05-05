6073d3d2ce4d6236c1e0d81b52890107298897cd
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2020-05-05 21:12:15 +0200

    Test with portal facetting looks ok.

--- poms-functional-tests-apis/src/test/java/nl/vpro/poms/npoapi/ApiPageSearchTest.java
+++ poms-functional-tests-apis/src/test/java/nl/vpro/poms/npoapi/ApiPageSearchTest.java
@@ -3,14 +3,17 @@ package nl.vpro.poms.npoapi;
 import lombok.extern.log4j.Log4j2;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.stream.Stream;
 
 import org.assertj.core.api.Assumptions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import nl.vpro.api.client.frontend.NpoApiClients;
+import nl.vpro.domain.api.TermFacetResultItem;
 import nl.vpro.domain.api.page.PageForm;
 import nl.vpro.domain.api.page.PageSearchResult;
 import nl.vpro.poms.ApiSearchTestHelper;
@@ -45,5 +48,26 @@ class ApiPageSearchTest extends AbstractSearchTest<PageForm, PageSearchResult> {
         PageSearchResult searchResultItems = clients.getPageService().find(form, null, "", 0L, 10);
         Assumptions.assumeThat(tester.apply(searchResultItems)).isTrue();
         test(name, searchResultItems);
+    }
+
+    @Test
+    void facetPortals() {
+        PageForm form = PageForm.builder()
+            .portalFacet()
+            .build();
+
+        PageSearchResult searchResultItems = clients.getPageService().find(form, null, "", 0L, 10);
+
+        List<TermFacetResultItem> portals = searchResultItems.getFacets().getPortals();
+        boolean allIdsEqualValue = true;
+        for (TermFacetResultItem p : portals) {
+            log.info("{}: {}", p.getValue(), p);
+            if (!p.getId().equals(p.getValue())) {
+                allIdsEqualValue = false;
+            }
+        }
+        assertThat(allIdsEqualValue).isFalse(); // for some this may be a fall back, but the 'value' should be the display value as provided by the portal service.
+
+
     }
 }
