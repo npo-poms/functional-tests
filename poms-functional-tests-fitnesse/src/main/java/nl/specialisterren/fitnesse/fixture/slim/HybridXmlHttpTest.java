ffb56fa0d806286190baa3dca1ef0f8848a2a6d5
Jesse Feddema <jessef@specialisterren.nl>
2020-06-26 13:07:15 +0200

    Refactor backend API tests: remove |show| workaround
    This commit adds the HybridXmlHttpTest class to allow using XmlHttpTest's
    methods without |get from|, |post to| etcetera failing on non-xml response
    bodies. Non-xml responses are allowed everywhere in all backend API tests
    for now, but that may be locally undone by calling |reject non-xml responses
    in subsequent requests|. Note that |xPath| and related functions will still
    throw an exception on non-xml responses, so tests will still fail where
    appropriate.
    
    The |show| workaround has been removed everywhere. In places where a
    workaround is still needed because the request call may return false due to
    an expected 4xx response code, comments have been added and |show| has been
    replaced with a more explicit |$ignoredResult=|. Also, these workarounds
    are contained in scenarios or table templates to hide the additional
    complexity from immediate view. Hopefully this will make things more
    readable for people less experienced with the ins and outs of FitNesse and
    HSAC's HttpTest fixtures.
    
    Additionally, some newline consistency fixes have been applied.

---
+++ poms-functional-tests-fitnesse/src/main/java/nl/specialisterren/fitnesse/fixture/slim/HybridXmlHttpTest.java
@@ -0,0 +1,42 @@
+package nl.specialisterren.fitnesse.fixture.slim;
+
+import nl.hsac.fitnesse.fixture.slim.XmlHttpTest;
+
+/**
+ * Extension of XmlHttpTest that adds functionality to allow non-xml responses
+ *
+ * XmlHttpTest returns false on getFrom, postTo and so on when the response
+ * has a body and that body is not xml. This is very inconvenient when testing
+ * APIs that return responses of various types. Therefore, this class allows
+ * turning that behavior on and off.
+ *
+ * Note that xPath and all related functions still throw an exception if the
+ * response body is not xml. Therefore, as long as you run at least 1 XPath
+ * after a request that should return an xml response, your test will still
+ * fail on receiving a non-xml response, even if you called
+ * acceptNonXmlResponsesInSubsequentRequests.
+ */
+public class HybridXmlHttpTest extends XmlHttpTest {
+    protected boolean acceptNonXmlResponses = false;
+
+    public void acceptNonXmlResponsesInSubsequentRequests() {
+        acceptNonXmlResponses = true;
+    }
+
+    public void rejectNonXmlResponsesInSubsequentRequests() {
+        acceptNonXmlResponses = false;
+    }
+
+    @Override
+    public boolean responseIsValid() {
+        if (acceptNonXmlResponses) {
+            // Return true or false as HttpTest would if throwExceptionOnHttpRequestFailure were false
+            // We rarely enable those exceptions anyway, and this way we avoid relying on implementation details,
+            // greatly reducing the probability that a HSAC update will break this override
+            int statusCode = responseStatus();
+            return !(statusCode < 100 || (statusCode >= 400 && statusCode <= 599));
+        } else {
+            return super.responseIsValid();
+        }
+    }
+}
