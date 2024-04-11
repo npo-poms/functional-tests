198af16822b3d57b9366aba636e1a69926727385
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2024-04-11 11:12:21 +0200

    added check for java version

--- start.sh
+++ start.sh
@@ -1,3 +1,12 @@
+
 java -version
+java_version=`java -version 2>&1 | fgrep -i version | cut -d'"' -f2 | sed -e 's/^1\./1\%/' -e 's/\..*//' -e 's/%/./'`
+
+if [[ $java_version -gt 11 ]]  ; then
+  echo "Java version $java_version must not be > 11"
+  exit 1
+fi
+
+
 
 mvn -DskipTests=false  compile dependency:copy-dependencies exec:exec
